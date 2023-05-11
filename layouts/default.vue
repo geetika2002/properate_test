e73b574fa61ba5cf06a447b8d439f512c1d3a870
Geetika Varma <gvarma@my.bcit.ca>
2023-05-11 13:05:26 -0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+          <NuxtLink to="/" class="font-bold">BCIT template</NuxtLink>
+          <ul class="flex gap-4">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/faq1">FAQ</NuxtLink></li>
+            <li><NuxtLink to="/products" class="btn">test</NuxtLink></li>
+            
+          </ul>
+          
+        </nav>
+      </header>
+      
+      <!-- output the page content-->
+      <div class="container mx-auto p-4">
+        <slot />
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  .router-link-exact-active {
+    color: #12b488;
+  }
+ 
+  </style>
+  
---
+++ pages/faq1.vue
@@ -0,0 +1,217 @@
+<template>
+
+<div class="container">
+    <h1>Frequently Asked Questions</h1>
+    <div class ="tab">
+        <input type="radio" name="acc" id="acc1">
+        <label for="acc1">
+            <h2>01</h2> 
+            <h3>Lorem ipsum dolor</h3> 
+        </label> 
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras convallis nunc vitae porttitor euismod. 
+             Mauris tincidunt eu enim ac pellentesque. Morbi gravida 
+            enim quis fermentum bibendum. Quisque eget erat 
+            at turpis condimentum pharetra. Sed consequat 
+            luctus leo sed elementum.</p>
+        </div>
+    </div> 
+
+    <div class ="tab">
+        <input type="radio" name="acc" id="acc2">
+        <label for="acc2">
+            <h2>02</h2> 
+            <h3>Lorem ipsum dolor</h3> 
+        </label> 
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras convallis nunc vitae porttitor euismod. 
+             Mauris tincidunt eu enim ac pellentesque. Morbi gravida 
+            enim quis fermentum bibendum. Quisque eget erat 
+            at turpis condimentum pharetra. Sed consequat 
+            luctus leo sed elementum.</p>
+        </div>
+    </div> 
+    
+    <div class ="tab">
+        <input type="radio" name="acc" id="acc3">
+        <label for="acc3">
+            <h2>03</h2> 
+            <h3>Lorem ipsum dolor</h3> 
+        </label> 
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras convallis nunc vitae porttitor euismod. 
+             Mauris tincidunt eu enim ac pellentesque. Morbi gravida 
+            enim quis fermentum bibendum. Quisque eget erat 
+            at turpis condimentum pharetra. Sed consequat 
+            luctus leo sed elementum.</p>
+        </div>
+    </div> 
+
+    <div class ="tab">
+        <input type="radio" name="acc" id="acc4">
+        <label for="acc4">
+            <h2>04</h2> 
+            <h3>Lorem ipsum dolor</h3> 
+        </label> 
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras convallis nunc vitae porttitor euismod. 
+             Mauris tincidunt eu enim ac pellentesque. Morbi gravida 
+            enim quis fermentum bibendum. Quisque eget erat 
+            at turpis condimentum pharetra. Sed consequat 
+            luctus leo sed elementum.</p>
+        </div>
+    </div> 
+
+    
+</div> 
+
+
+  </template>
+  
+  <style scoped>
+
+* {
+margin: 0; 
+padding: 0; 
+box-sizing: border-box; 
+font-family: 'Roboto';  
+}
+
+.container{
+    margin: 0 40px; 
+    max-width: 600px; 
+    display: flex; 
+    flex-direction: column; 
+    gap: 20px; 
+}
+
+.container h1 {
+    color: #333; 
+}
+
+.container .tab {
+    position: relative; 
+    background: #fff; 
+    padding: 0 20px 20px; 
+    box-shadow: 0 15px 25px rgba(0,0,0,0.05);
+    border-radius: 5px; 
+    overflow: hidden; 
+}
+
+.container .tab input {
+    appearance: none; 
+
+}
+
+.container .tab label {
+    display: flex; 
+    align-items: center; 
+    cursor: pointer;
+}
+
+.container .tab label::after
+{
+    content: '+'; 
+    position: absolute; 
+    right: 20px; 
+    font-size: 2em; 
+    color: rgba(0,0,0,0.1);
+    transition: transform 1s; 
+}
+
+.container .tab:hover label::after{
+    color: #333; 
+}
+
+.container .tab input:checked ~ label::after {
+    transform: rotate(135deg);
+    color: #fff; 
+}
+
+.container .tab label h2 {
+    width: 40px; 
+    height: 40px; 
+    background: #333; 
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    color: #fff; 
+    font-size: 1.25em; 
+    border-radius: 5px; 
+    margin-right: 10px; 
+}
+
+.container .tab input:checked ~ label h2 {
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    width: 100%;
+    height: 100%;
+    color: rgba(255,255,255, 0.2); 
+    font-size: 8em; 
+    justify-content: flex-end;
+    padding: 20px; 
+}
+
+.container .tab:nth-child(2) label h2{
+    background: linear-gradient(135deg, #70f570, #49c628);
+}
+
+.container .tab:nth-child(3) label h2{
+    background: linear-gradient(135deg, #3c6ce7, #00eaff);
+}
+
+.container .tab:nth-child(4) label h2{
+    background: linear-gradient(135deg, #ff96f9, #c32bac);
+}
+
+.container .tab:nth-child(5) label h2{
+    background: linear-gradient(135deg, #fd6e6a, #ffc600);
+}
+
+.container label h3 {
+    position: relative; 
+    font-weight: 500; 
+    color: #333; 
+    z-index: 10; 
+}
+
+.container .tab input:checked ~ label h3 {
+background: #fff; 
+padding: 2px 10px; 
+color: #333; 
+border-radius: 2px; 
+box-shadow: 0px 5px 15px rgba(0,0,0,0.05);
+
+}
+
+.container .tab .content {
+    max-height: 0;
+    transition: 1s; 
+    overflow: hidden; 
+}
+
+.container .tab input:checked ~ .content{
+    max-height: 100vh; 
+}
+
+.container .tab .content p {
+    position: relative;
+    padding: 10px 0; 
+    color: #333; 
+    z-index: 10; 
+}
+
+.container .tab input:checked ~ .content p {
+    color: #fff; 
+
+}
+
+
+
+
+</style>
